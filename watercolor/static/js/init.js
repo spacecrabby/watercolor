58cd6cb52fd6beb367a8174c9cc497435c797938
belskikh <belskikh.aleksande@gmail.com>
2016-04-11 23:39:36 +0300

    fixed links names

--- watercolor/static/js/init.js
+++ watercolor/static/js/init.js
@@ -11,16 +11,16 @@
 $('a').click(function(){
     var contact = "#contact";
     if (+this.href.indexOf(contact) > -1) {
-        $("html, body").animate({ scrollTop: $(document).height() }, 500)
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow")
         return false;
     };
 
     if ($( $.attr(this, 'href') ).offset() === undefined) {
-        $("html, body").animate({ scrollTop: 0 }, 500);
+        $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
     }
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top - 64,
-    }, 500);
+    }, "slow");
     return false;
 });
