189f7ad0ce4518e129b8e1fbaf038140e8464d10
belskikh <belskikh.aleksande@gmail.com>
2016-04-16 21:36:11 +0300

    using forms as django forms

--- landing/forms.py
+++ landing/forms.py
@@ -1,2 +1,19 @@
 from django import forms
 
+
+class ContactForm(forms.Form):
+    contact_name = forms.CharField(
+        required=True,
+        label='Имя',
+        widget=forms.TextInput(
+            attrs={'class': 'validate'},
+        )
+    )
+    contact_email = forms.EmailField(required=True)
+    contact_phone = forms.CharField(required=False)
+    message = forms.CharField(
+        required=True,
+        widget=forms.Textarea(
+            attrs={'class': "materialize-textarea"},
+        )
+    )
--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from landing.forms import ContactForm
 # Create your views here.
 
 
 def landing(request):
-    return render(request, 'landing.html')
+    form_class = ContactForm
+
+    return render(request, 'landing.html',
+                  {'form': form_class})
