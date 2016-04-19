75f1b1c1261072ecfd934c8a0c7770607ffa346e
belskikh <belskikh.aleksande@gmail.com>
2016-04-19 19:56:06 +0300

    changed images in preview

--- static/landing/js/lightbox.js
+++ static/landing/js/lightbox.js
@@ -310,7 +310,7 @@
     }
   };
 
-  // Display the image and its details and begin preload neighboring images.
+  // Display the image and its details and begin preload neighboring img.
   Lightbox.prototype.showImage = function() {
     this.$lightbox.find('.lb-loader').stop(true).hide();
     this.$lightbox.find('.lb-image').fadeIn('slow');
@@ -391,7 +391,7 @@
     });
   };
 
-  // Preload previous and next images in set.
+  // Preload previous and next img in set.
   Lightbox.prototype.preloadNeighboringImages = function() {
     if (this.album.length > this.currentImageIndex + 1) {
       var preloadNext = new Image();
