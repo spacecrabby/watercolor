bb63d1b411a952c9d91353b5f8bb0a742d5d64cd
belskikh <belskikh.aleksandr@gmail.com>
2016-04-23 19:46:41 +0300

    tel numvers

--- static/landing/js/init.js
+++ static/landing/js/init.js
@@ -8,18 +8,20 @@
 
 
 $('a').click(function(){
-    var contact = "#contact";
-    if (+this.href.indexOf(contact) > -1) {
-        $("html, body").animate({ scrollTop: $(document).height() }, "slow")
-        return false;
-    };
+    if (+this.href.indexOf('tel') > -1) {
+        var contact = "#contact";
+        if (+this.href.indexOf(contact) > -1) {
+            $("html, body").animate({ scrollTop: $(document).height() }, "slow")
+            return false;
+        };
 
-    if ($( $.attr(this, 'href') ).offset() === undefined) {
-        $("html, body").animate({ scrollTop: 0 }, "slow");
+        if ($( $.attr(this, 'href') ).offset() === undefined) {
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+            return false;
+        }
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 64,
+        }, "slow");
         return false;
     }
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top - 64,
-    }, "slow");
-    return false;
 });
