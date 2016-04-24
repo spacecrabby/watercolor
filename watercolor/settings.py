0a78e25749b9f933a085db861526a72973596b8a
belskikh <belskikh.aleksandr@gmail.com>
2016-04-24 14:31:22 +0300

    added new email settings

--- watercolor/settings.py
+++ watercolor/settings.py
@@ -136,8 +136,8 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Email settings
 
-EMAIL_HOST_USER = 'watercolorsketching'
+EMAIL_HOST_USER = 'info@watercolorsketching.ru'
 EMAIL_HOST_PASSWORD = 'HandMade1992'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.locum.ru'
+EMAIL_PORT = 25
 EMAIL_USE_TLS = True
