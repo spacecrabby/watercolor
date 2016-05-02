792d9962da7e7e2ae98ee779f454c7a7cc8ef247
belskikh <belskikh.aleksande@gmail.com>
2016-05-02 20:29:26 +0300

    xzczxc

--- watercolor/urls.py
+++ watercolor/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.conf import settings
 from django.contrib import admin
 from landing import views
 
@@ -11,3 +12,9 @@ urlpatterns = [
     url(r'^$', views.landing, name='landing'),
     url(r'^success', views.success, name='success')
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
